0cf2819df3cb5def787200ab5eca0ebea7a64a6d
Hamza Qureshi <hamza7771.861@gmail.com>
2026-01-22 14:42:50 +0500

    refactor

---
+++ targets/cpu/LLVMBackend.cpp
@@ -0,0 +1,19 @@
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "Passes.h"
+
+namespace mlir
+{
+    namespace mlp
+    {
+        namespace hardware
+        {
+
+            void registerCPUPasses()
+            {
+            }
+
+        } // namespace cpu
+    } // namespace ppytorch
+} // namespace mlir
---
+++ targets/riscv/Backend.cpp
@@ -0,0 +1,19 @@
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "Passes.h"
+
+namespace mlir
+{
+    namespace mlp
+    {
+        namespace hardware
+        {
+
+            void registerRISCVPasses()
+            {
+            }
+
+        } // namespace cpu
+    } // namespace ppytorch
+} // namespace mlir
